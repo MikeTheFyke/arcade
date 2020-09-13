d428bd928438e7777211aee8cc415aa68f41a2c5
MikeTheFyke <mikefyke@hotmail.com>
2020-09-13 16:56:37 -0400

    created viewport object to decide where the viewport should be centered and what to be drawn

--- public/logic/tileBased.js
+++ public/logic/tileBased.js
@@ -44,6 +44,40 @@ var gameMap = [ // Tile 0 barrier, Tile 1 path
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0  // Row 20
 ];
 
+var viewport = { // This Object will help to decide what will be viewable to the player
+    screen : [0,0],
+    startTile : [0,0],
+    endTile : [0,0],
+    offset: [0,0],
+    update : function(px, py){
+        this.offset[0] = Math.floor((this.screen[0]/2) - px);
+        this.offset[1] = Math.floor((this.screen[1]/2) - py);
+
+        var tile = [ // dead center of viewing tileset
+            Math.floor(px / tileW),
+            Math.floor(py / tileH)
+        ];
+        this.startTile[0] = tile[0] - 1 - Math.ceil((this.screen[0]/2) / tileW);
+        this.startTile[1] = tile[1] - 1 - Math.ceil((this.screen[1]/2) / tileH);
+        
+        if(this.startTile[0] < 0) {
+            this.startTile[0] = 0;
+        }
+        if(this.startTile[1] < 0) {
+            this.startTile[1] = 0;
+        }
+        this.endTile[0] = tile[0] + 1 + Math.ceil((this.screen[0]/2)/ tileW);
+        this.endTile[1] = tile[1] + 1 + Math.ceil((this.screen[1]/2)/ tileH);
+
+        if(this.endTile[0] >= mapW) {
+            this.endTile[0] = mapW - 1;
+        }
+        if(this.endTile[1] >= mapH) {
+            this.endTile[1] = mapH - 1;
+        }
+    }
+}
+
 function Character(){
     this.tileFrom = [1,1];
     this.tileTo =  [1,1];
