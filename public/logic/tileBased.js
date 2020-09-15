c0ee87903f253f4b0b5b482e540fced655e612a6
MikeTheFyke <mikefyke@hotmail.com>
2020-09-15 15:46:32 -0400

    Added grass to list of tileTypes

--- public/logic/tileBased.js
+++ public/logic/tileBased.js
@@ -76,7 +76,7 @@ var floorTypes = {
 
 var tileTypes = {
     0 : {color: "#685b48", floor:floorTypes.solid, sprite:[{x:0,   y:0, w:40, h:40}]},
-    1 : {color: "#5aa457", floor:floorTypes.path,  sprite:[{x:40,  y:0, w:40, h:40}]},
+    1 : {color: "#5aa457", floor:floorTypes.grass,  sprite:[{x:40,  y:0, w:40, h:40}]},
     2 : {color: "#e8bd7a", floor:floorTypes.path,  sprite:[{x:80,  y:0, w:40, h:40}]},
     3 : {color: "#286625", floor:floorTypes.solid, sprite:[{x:120, y:0, w:40, h:40}]},
     4 : {color: "#678fd9", floor:floorTypes.water, sprite:[{x:160, y:0,  w:40, h:40, d:200}, {x:200, y:0,  w:40, h:40, d:200},
