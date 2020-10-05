f0e1c4c40edc440911ec59e3fe685d768db24643
MikeTheFyke <mikefyke@hotmail.com>
2020-10-05 11:31:07 -0400

    Declaring canvas contex rows and collums

--- public/logic/tetrisLogic.js
+++ public/logic/tetrisLogic.js
@@ -1,5 +1,12 @@
-const canvasWidth = 300;
-const canvasHeight = 600;
+const canvas = document.getElementById('tetrisCanvas');
+const ctx = canvas.getContext('2d');
+
+const canvasWidth = canvas.width;
+const canvasHeight = canvas.clientHeight;
 
 const numberOfRows = 20;
 const numberOfCols = 10;
+
+const cellSize = canvasWidth / numberOfCols;
+const borderSize = 0.2;
+
