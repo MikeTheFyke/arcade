a77b6fb5a737c69151c074a4b60490893ccbb62d
MikeTheFyke <mikefyke@hotmail.com>
2020-10-05 11:52:05 -0400

    Created block class

--- public/logic/tetrisLogic.js
+++ public/logic/tetrisLogic.js
@@ -10,3 +10,27 @@ const numberOfCols = 10;
 const cellSize = canvasWidth / numberOfCols;
 const borderSize = 0.2;
 
+class  Block {
+    constructor(cells, x, y) {
+        this.cells = cells
+        this.position = {x, y}
+        this.isAlive = true
+    }
+}
+
+const render = (game, block, time) => {
+
+}
+
+const generateField = (rows, cols) => {
+    const field = Array.from({length: rows},
+        () => Array.from({length: cols}, () => 0))
+}
+
+window.onload = () => {
+    const game = {
+        ctx,
+        field: generateField(numberOfRows + 4, numberOfCols),
+    }
+ render(game)
+}
